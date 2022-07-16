b0e8dc7b67adb257649e2fb73be5d91bf77d53d8
Alex Kitt <alex.kitt@outlook.com>
2022-07-16 19:41:02 -0400

    Theoretical interactions between fire, earth, and air

--- Assets/Code/Flammable.cs
+++ Assets/Code/Flammable.cs
@@ -21,9 +21,16 @@ public class Flammable : MonoBehaviour
                 fireTimer = maxFireDuration;
             }
         }
-        else if (col.gameObject.CompareTag("Water Element") && fireTimer > 0)
+        else if (fireTimer > 0)
         {
-            fireTimer = 0;
+            if (col.gameObject.CompareTag("Water Element") || col.gameObject.CompareTag("Earth Element"))
+            {
+                fireTimer = 0;
+            }
+            else if (col.gameObject.CompareTag("Air Element"))
+            {
+                spreadFireChance *= 2;
+            }
         }
     }
 
