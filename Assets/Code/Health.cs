e5e24fb1d191a57471da6c5e88672e3ca66964a7
Jake Gianola <jakegianola204@gmail.com>
2022-07-16 16:59:31 -0700

    Enemy Melee sorta working, player invinciblity flash broken

--- Assets/Code/Health.cs
+++ Assets/Code/Health.cs
@@ -3,20 +3,40 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     public float maxHealth;
-
-    private float currentHealth;
-
+    private bool isPlayer = false;
+    public float currentHealth;
+    private GameObject player;
     private void Awake()
     {
+        isPlayer = gameObject.CompareTag("Player");
+        if (isPlayer)
+        {
+            player = gameObject;
+        }
         currentHealth = maxHealth;
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
+        if (isPlayer)
+        {
+            Player.Instance.InvFrames();
+        }
+        
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    public float getCurrentHealth()
+    {
+        return currentHealth;
+    }
+    
+    public void setHealth(float hp)
+    {
+        currentHealth = hp;
     }
 }
