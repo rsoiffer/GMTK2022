72cbb06e52608fcf5eb041a3427df4f9bcf4ee3c
rsoiffer <rsoiffer@gmail.com>
2022-07-16 15:05:10 -0400

    Added fire stream particles

--- Assets/Code/PlayerFire.cs
+++ Assets/Code/PlayerFire.cs
@@ -5,6 +5,13 @@ public class PlayerFire : MonoBehaviour
     public GameObject fireball;
     public float shootSpeed;
 
+    public GameObject fireStreamParticles;
+    public GameObject fireStreamDamageArea;
+    public float minStreamTime = .2f;
+
+    private float lastMouseDownTime;
+    private bool fireStreamActive;
+
     private Vector2 ToMouse()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -14,11 +21,42 @@ public class PlayerFire : MonoBehaviour
 
     private void Update()
     {
+        fireStreamActive = false;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMouseDownTime = Time.time;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time > lastMouseDownTime + minStreamTime)
+            {
+                // Fire stream
+                fireStreamActive = true;
+                fireStreamParticles.transform.rotation = Quaternion.Euler(0, 0,
+                    Mathf.Rad2Deg * Mathf.Atan2(ToMouse().y, ToMouse().x));
+                fireStreamDamageArea.transform.rotation = Quaternion.Euler(0, 0,
+                    Mathf.Rad2Deg * Mathf.Atan2(ToMouse().y, ToMouse().x));
+            }
+        }
+
         if (Input.GetMouseButtonUp(0))
         {
-            var newFireball = Instantiate(fireball);
-            newFireball.transform.position = transform.position;
-            newFireball.GetComponent<Rigidbody2D>().velocity = shootSpeed * ToMouse().normalized;
+            if (Time.time < lastMouseDownTime + minStreamTime)
+            {
+                // Fireball
+                var newFireball = Instantiate(fireball);
+                newFireball.transform.position = transform.position;
+                newFireball.GetComponent<Rigidbody2D>().velocity = shootSpeed * ToMouse().normalized;
+            }
+        }
+
+        fireStreamDamageArea.SetActive(fireStreamActive);
+        foreach (var particles in fireStreamParticles.GetComponentsInChildren<ParticleSystem>())
+        {
+            var emission = particles.emission;
+            emission.enabled = fireStreamActive;
         }
     }
 }
