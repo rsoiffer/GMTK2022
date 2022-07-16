867f0c084e8bf3b233bd1987740e1ff97d172123
Alex Kitt <alex.kitt@outlook.com>
2022-07-16 14:03:00 -0400

    Basic skeleton for fire

---
+++ Assets/Chemistry/ChemistryFire.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ChemistryFire : ChemistryElement
+{
+    public override void InteractWith(ChemistryMaterial material)
+    {
+        
+    }
+    
+    public override void InteractWith(ChemistryElement element)
+    {
+        
+    }
+}
