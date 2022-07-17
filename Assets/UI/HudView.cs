49b1ddca2825930fdac9f31c95fa911647b5954c
Sarah Marshall <sarah@sarahmarshall.name>
2022-07-17 00:01:32 -0700

    Show hearts in health bar

--- Assets/UI/HudView.cs
+++ Assets/UI/HudView.cs
@@ -1,8 +1,10 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class HudView : MonoBehaviour
 {
+    [SerializeField] private VisualTreeAsset heartTemplate;
     private VisualElement healthBar;
 
     private void Start()
@@ -13,17 +15,36 @@ public class HudView : MonoBehaviour
 
     private void Update()
     {
-        float healthRatio;
+        float currentHealth;
         if (Player.Instance == null)
         {
-            healthRatio = 0;
+            currentHealth = 0;
         }
         else
         {
             var health = Player.Instance.GetComponent<Health>();
-            healthRatio = health.CurrentHealth / health.maxHealth;
+            currentHealth = health.CurrentHealth;
+            FillHearts(Mathf.CeilToInt(health.maxHealth));
         }
 
-        healthBar.style.width = new StyleLength(Length.Percent(100 * healthRatio));
+        foreach (var (heart, index) in healthBar.Children().Select((h, i) => (h, i)))
+        {
+            var fullness = currentHealth - index;
+            heart.Q("Full").style.display = fullness >= 1.0 ? DisplayStyle.Flex : DisplayStyle.None;
+            heart.Q("Half").style.display = fullness >= 0.5 && fullness < 1.0 ? DisplayStyle.Flex : DisplayStyle.None;
+            heart.Q("Empty").style.display = fullness < 0.5 ? DisplayStyle.Flex : DisplayStyle.None;
+        }
+    }
+
+    private void FillHearts(int hearts)
+    {
+        if (hearts != healthBar.childCount)
+        {
+            healthBar.Clear();
+            foreach (var _ in Enumerable.Range(0, hearts))
+            {
+                healthBar.Add(heartTemplate.Instantiate());
+            }
+        }
     }
 }
