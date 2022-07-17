e5b89455274b2972577ff0bfb8ad786e0bfcbbe0
rsoiffer <rsoiffer@gmail.com>
2022-07-17 02:04:34 -0400

    Added fire upgrades

--- Assets/Code/LevelManager.cs
+++ Assets/Code/LevelManager.cs
@@ -7,6 +7,11 @@ public class LevelManager : MonoBehaviour
 
     public int levelNum;
 
+    public int upgradeFire1;
+    public int upgradeFire2;
+    public int upgradeFire3;
+    public int upgradeFire4;
+
     private int choice = -1;
 
     private void Awake()
@@ -23,6 +28,25 @@ public class LevelManager : MonoBehaviour
 
     public void ToNextLevel()
     {
+        switch (choice)
+        {
+            case -1:
+                // TODO
+                break;
+            case 0:
+                upgradeFire1++;
+                break;
+            case 1:
+                upgradeFire2++;
+                break;
+            case 2:
+                upgradeFire3++;
+                break;
+            case 3:
+                upgradeFire4++;
+                break;
+        }
+
         levelNum += 1;
         SceneManager.LoadScene("Level");
     }
